f728a3664a9c3a598252628932bfe1ecfbd557f4
segz7448 <segz7448@gmail.com>
2026-06-28 22:19:19 -0700

    fix: remove NewArch/Expo imports from MainActivity - use standard DefaultReactActivityDelegate

--- android/app/src/main/java/com/mobilecloud/MainActivity.kt
+++ android/app/src/main/java/com/mobilecloud/MainActivity.kt
@@ -5,24 +5,17 @@ import android.os.Build
 import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
 
     override fun getMainComponentName(): String = "mobilecloud"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
-        ReactActivityDelegateWrapper(
-            this,
-            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-            object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {}
-        )
+        DefaultReactActivityDelegate(this, mainComponentName)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(null)
-        // Start background service on app launch
         startCloudService()
     }
 
